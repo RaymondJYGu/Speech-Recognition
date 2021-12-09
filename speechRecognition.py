0c1a1a368643568e7675dc394525bca99b65845d
HowardCai1 <47042307+HowardCai1@users.noreply.github.com>
2021-12-09 14:09:27 -0600

    Add files via upload

---
+++ speechRecognition.py
@@ -0,0 +1,48 @@
+import os
+import librosa
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from keras.models import load_model
+
+from keras.models import Model
+
+
+model= load_model('best_model.hdf5')
+
+
+
+classes= ['bed', 'bird', 'cat', 'dog', 'down', 'eight', 'five', 'four', 'go', 'happy', 'house', 'left', 'marvin', 'nine', 'no', 'off', 'on', 'one', 'right', 'seven', 'sheila', 'six', 'stop', 'three', 'tree', 'two', 'up', 'wow', 'yes', 'zero']
+
+
+def predict(audio):
+    prob=model.predict(audio.reshape(1,8000,1))
+    index=np.argmax(prob[0])
+    return classes[index]
+
+import sounddevice as sd
+import soundfile as sf
+
+samplerate = 16000
+duration = 1 # seconds
+filename = 'fun.wav'
+print("start")
+mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+    channels=1, blocking=True)
+print("end")
+sd.wait()
+sf.write(filename, mydata, samplerate)
+
+
+samples, sample_rate = librosa.load('fun.wav', sr = 16000)
+samples = librosa.resample(samples, sample_rate, 8000)
+ipd.Audio(samples,rate=8000)
+
+
+
+print(predict(samples))
